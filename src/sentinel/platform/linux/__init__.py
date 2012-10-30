574e8dcce5a61ed81bfb25afbc33a07f62fb401f
Dong-seob Park <dongseob.park@gmail.com>
2012-10-31 02:19:59 +0900

    on coding proc filesystem

--- src/sentinel/platform/linux/__init__.py
+++ src/sentinel/platform/linux/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 PROC_CPUINFO_FILE = '/proc/cpuinfo'
 PROC_MEMINFO_FILE = '/proc/meminfo'
 PROC_STAT_FILE = '/proc/stat'
@@ -9,11 +11,56 @@ PROC_MDSTAT_FILE = '/proc/mdstat'
 PROC_LOADAVG_FILE = '/proc/loadavg'
 PROC_VERSION_FILE = '/proc/version'
 
-def cpu_status():
-    pass
+cpu_regex = re.compile(r'^cpu[0-9]+')
 
-def uptime():
-    pass
+def system_version(system_status):
+    with open(PROC_VERSION_FILE, 'r') as f:
+        line = f.readline()
+        data = line.split(' ')
+        system_status.os_type, system_status.os_version = data[0], data[1]
 
-def version():
-    version_str = open(PROC_VERSION_FILE, 'r').read()
+def load_average(system_status):
+    with open(PROC_LOADAVG_FILE, 'r') as f:
+        line = f.readline()
+        data = line.split(' ')
+        system_status.loadavg1, system_status.loadavg5, system_status.loadavg15 = data[0], data[1], data[2]
+
+def uptime(system_status):
+    with open(PROC_UPTIME_FILE, 'r') as f:
+        line = f.readline()
+        data = line.split(' ')
+        system_status.uptime, system_status.idletime = data[0], data[1]
+
+class LinuxCPU:
+    def __init__(self, user, nice, system, idle, iowait, irq, softirq):
+        self.user = user
+        self.nice = nice
+        self.system = system
+        self.idle = idle
+        self.iowait = iowait
+        self.irq = irq
+        self.softirq = softirq
+
+    def __str__(self):
+        return "%d %d %d %d %d %d %d" % (self.user, self.nice, self.system, self.idle, self.iowait, self.irq, self.softirq)
+
+def get_cpu_status(line):
+    data = line.split(' ')
+    cpu = LinuxCPU(int(data[2]), int(data[3]), int(data[4]), int(data[5]), int(data[6]), int(data[7]), int(data[8]))
+    print(cpu)
+    return 100 * cpu.idle / (cpu.user + cpu.nice + cpu.system + cpu.idle)
+
+def get_cpun_status(line):
+    data = line.split(' ')
+    cpu = LinuxCPU(int(data[1]), int(data[2]), int(data[3]), int(data[4]), int(data[5]), int(data[6]), int(data[7]))
+    print(cpu)
+    return 100 * cpu.idle / (cpu.user + cpu.nice + cpu.system + cpu.idle)
+
+def cpu_status(system_status):
+    with open(PROC_STAT_FILE, 'r') as f:
+        line = f.readline()
+        system_status.cpu_total = get_cpu_status(line)
+        system_status.cpu_usages = []
+        for line in f.readlines():
+            if cpu_regex.match(line):
+                system_status.cpu_usages.append(get_cpun_status(line))
--- src/sentinel/worker.py
+++ src/sentinel/worker.py
@@ -1,13 +1,31 @@
 import time
 
+monitor_attrs = ['uptime', 'load_average', 'cpu_status']
+
+class SystemStatus:
+    def __init__(self):
+        self.os_type = ''
+        self.os_version = ''
+        self.uptime = 0
+        self.idletime = 0
+        self.cpu_total = None
+        self.cpu_usages = []
+
 class SystemMonitor:
     def __init__(self, platform_api):
         self.platform_api = platform_api
 
     def __call__(self):
+        system_status = SystemStatus()
+        if hasattr(self.platform_api, 'system_version'):
+            api_func = getattr(self.platform_api, 'system_version')
+            api_func(system_status)
         while(True):
             time.sleep(1)
-            if hasattr(self.platform_api, 'cpu_status'):
-                self.platform_api.cpu_status()
-            if hasattr(self.platform_api, 'version'):
-                self.platform_api.version()
+            for attr in monitor_attrs:
+                if hasattr(self.platform_api, attr):
+                    api_func = getattr(self.platform_api, attr)
+                    api_func(system_status)
+            print(system_status.cpu_total)
+            for cpu in system_status.cpu_usages:
+                print(cpu)
